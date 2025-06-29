6c95bde157759a0440901ae00e848fe4d37f3709
Ho3einMokhtari <hosseinmokhtari5555@gmail.com>
2025-06-29 13:32:38 +0330

    test

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ from fastapi.responses import Response
 import helper
 import json
 import config
+from config import plan_names, amounts
 
 app = FastAPI()
 
@@ -114,40 +115,108 @@ async def get_service_iphone(data_limit, expire):
 
 
 @app.get("/get-service-rep/")
-async def get_service_rep(number, user_id, rep_code, user_inventory):
+async def get_service_rep(number: int, user_id: int, rep_code: str):
     db_path = helper.get_db_path()
     # find_rep = helper.execute_with_retry(db_path=db_path, query=f"SELECT * FROM users WHERE rep_code = '{rep_code}'")
+    user_inventory = None
+    result = None
     with helper.sqlite_connection(db_path) as conn:
         cursor = conn.cursor()
         result = cursor.execute(f"SELECT * FROM users WHERE rep_code='{rep_code}'").fetchone()
         conn.commit()
-        print(result)
+        if result is None:
+            return Response(json.dumps({"status": 404, "message": "user not found"}), 404)
+        user_inventory = result[1]
+    service_price = amounts[number] // 2
+    if (user_inventory - service_price) < 0:
+        return Response(json.dumps({"status": 403, "message": "inventory is none!"}), 403)
+    try:
 
-    # print(find_rep.fetchone())
-#     try:
+        username, password, client_id = await helper.get_service(num=int(number), user_id=user_id)
+        if username is None or password is None:
+            response = {
+                "status": 500,
+            }
+            return Response(json.dumps(response), 500)
+        else:
+            response = {
+                "status": 200,
+                "username": username,
+                "password": password,
+            }
+            plan_name = plan_names[number]
+            rep_after_inventory = user_inventory - service_price
+            cursor.execute(f"UPDATE users SET inventory={rep_after_inventory} WHERE rep_code='{rep_code}'")
+            conn.commit()
+            text = f"""📣 جزئیات ساخت اکانت در ربات نماینده شما ثبت شد .
+▫️آیدی عددی کاربر : {user_id}
+▫️آیدی عددی نماینده : {result[0]}
+▫️نام کاربری کانفیگ :{username}
+▫️رمز عبور کانفیگ :{password}
+▫️پلن سرویس : {plan_name}
+▫️موجودی نماینده قبل از خرید :{user_inventory} تومان
+▫️موجودی نماینده قبل از خرید : {rep_after_inventory}"""
+            await helper.send_telegram_message(config.REPORT_CHANNEL_ID, text)
+            return Response(json.dumps(response), 200)
+    except Exception as e:
+        print(e)
+        response = {
+            "status": 500,
+        }
+        return Response(json.dumps(response), 500)
 
-#         username, password, client_id = await helper.get_service(num=int(number), user_id=user_id)
-#         if username is None or password is None:
-#             response = {
-#                 "status": 500,
-#             }
-#             return Response(json.dumps(response), 500)
-#         else:
-#             response = {
-#                 "status": 200,
-#                 "username": username,
-#                 "password": password,
-#             }
-#             text = f"""✅ #سرویس جدید
-# ➖➖➖➖➖➖➖➖➖
-# 👤نام کاربری : {username}
-# 🔑پسورد : {password}
-# """
-#             await helper.send_telegram_message(config.REPORT_CHANNEL_ID, text)
-#             return Response(json.dumps(response), 200)
-#     except Exception as e:
-#         print(e)
-#         response = {
-#             "status": 500,
-#         }
-#         return Response(json.dumps(response), 500)
+
+@app.get("/service-extension-rep/")
+async def service_extension_rep(number: int, username: str, rep_code: str, user_id: int):
+    db_path = helper.get_db_path()
+    # find_rep = helper.execute_with_retry(db_path=db_path, query=f"SELECT * FROM users WHERE rep_code = '{rep_code}'")
+    user_inventory = None
+    result = None
+    with helper.sqlite_connection(db_path) as conn:
+        cursor = conn.cursor()
+        result = cursor.execute(f"SELECT * FROM users WHERE rep_code='{rep_code}'").fetchone()
+        conn.commit()
+        if result is None:
+            return Response(json.dumps({"status": 404, "message": "user not found"}), 404)
+        user_inventory = result[1]
+    service_price = amounts[number] // 2
+    if (user_inventory - service_price) < 0:
+        return Response(json.dumps({"status": 403, "message": "inventory is none!"}), 403)
+    try:
+        status = await helper.service_extension(plan_id=int(number), username=username)
+        if status is None:
+            response = {
+                "status": 500,
+            }
+            return Response(json.dumps(response), 500)
+        else:
+            if status == 500:
+                response = {
+                    "message": "an error to service extension",
+                    "status": 500,
+                }
+                return Response(json.dumps(response), 500)
+            else:
+                response = {
+                    "message": "plan added to user service!",
+                    "status": 200,
+                }
+                plan_name = plan_names[number]
+                rep_after_inventory = user_inventory - service_price
+                cursor.execute(f"UPDATE users SET inventory={rep_after_inventory} WHERE rep_code='{rep_code}'")
+                conn.commit()
+                text = f"""📣 جزئیات تمدید سرویس در ربات نماینده شما ثبت شد .
+                ▫️آیدی عددی کاربر : {user_id}
+                ▫️آیدی عددی نماینده : {result[0]}
+                ▫️نام کاربری کانفیگ :{username}
+                ▫️پلن سرویس : {plan_name}
+                ▫️موجودی نماینده قبل از خرید :{user_inventory} تومان
+                ▫️موجودی نماینده قبل از خرید : {rep_after_inventory}"""
+                await helper.send_telegram_message(config.REPORT_CHANNEL_ID, text)
+                return Response(json.dumps(response), 200)
+    except Exception as e:
+        print(e)
+        response = {
+            "status": 500,
+        }
+        return Response(json.dumps(response), 500)
